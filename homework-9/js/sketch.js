036967ccc2886bf6d98ad30e091698c767cc1787
Stormy Hack <65190874+stormyhack@users.noreply.github.com>
2021-03-16 12:42:11 -0600

    commited

---
+++ homework-9/js/sketch.js
@@ -0,0 +1,20 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+function draw()
+{
+    background(120, 45, 78);
+    circle(250, 165, 175);
+
+    rect(175, 260, 150, 225);
+
+    point(225, 150);
+    point(265, 150);
+    stroke('black');
+    strokeWeight(10);
+
+    line(220, 190, 270, 190);
+
+    triangle(50, 95, 78, 40, 106, 95);
+}
